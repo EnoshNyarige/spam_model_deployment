746df4b925a7a409ba2400081a1c692bde3fbf3b
EnoshNyarige <enyarige17@alustudent.com>
2020-12-07 13:49:05 +0200

    empty push

--- app.py
+++ app.py
@@ -13,8 +13,11 @@ html_temp = """
 	<h3 style="color:white;text-align:center;">Machine Learning</h3>
 	</div>
 	<div>
-	<p style="color:white;text-align:center;">\n\nDeogratius Amani \n\nYves Mugenga \n\nEnosh Nyarige</p>
-	</div>
+	<h3 style="color:black;text-align:center;">Members</h3>
+	<p style="color:black;text-align:center;">Deogratius Amani</p> 
+	<p style="color:black;text-align:center;">Yves Mugenga</p> 
+	<p style="color:black;text-align:center;">Enosh Nyarige</p>
+	
 	"""
 st.markdown(html_temp,unsafe_allow_html=True)
 
@@ -27,16 +30,16 @@ tokenizer = pickle.load(open(tokenizer_file, "rb"))
 
 user_input = [user_input.split(" ")]
 text_seq = tokenizer.texts_to_sequences(user_input)
-padded_text_seq = pad_sequences(text_seq, maxlen=4, padding="post") 
+padded_text_seq = pad_sequences(text_seq, maxlen=6, padding="post") 
 
-# Open the saved model
-model_file = "spam_model.h5"
+# 
+model_file = "model.h5"
 bilstm_model = load_model(model_file, compile = False)
 
 y_pred = bilstm_model.predict(padded_text_seq)
 y_pred = np.argmax(y_pred, axis=1)
 
-if st.button("Classify"):
+if st.button("Predict"):
     if y_pred[0] == 0:
         st.write("The classifier says this a **Ham** email. No need for more caution")
     elif y_pred[0] == 1:
